c9901af0bd8d1600c0e0d2f143373bfc750f052e
Rodrigo Valdés <rodrigovaldes@gmail.com>
2020-01-02 00:01:55 -0300

    Commit Inicial

---
+++ src/main/java/com/cybersyn/jpapg/JpaPgApplication.java
@@ -0,0 +1,13 @@
+package com.cybersyn.jpapg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersyn/jpapg/JpaPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersyn.jpapg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
